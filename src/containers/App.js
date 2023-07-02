9825b6b8f6a9cec893ac5e4d7add85530cf7b0d0
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-07-02 10:22:27 -0600

    removes unused variable

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,39 +7,41 @@ import { ErrorBoundary } from "../components/ErrorBoundary";
 const Fragment = React.Fragment;
 
 export class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ""
-		};
-	}
-	componentDidMount() {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then(response => response.json())
-			.then(users => {
-				this.setState({ robots: users });
-			});
-	}
-	onSearchChange = e => {
-		this.setState({ searchfield: e.target.value });
-	};
-	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(searchfield.toLowerCase()));
-		// console.log(filteredRobots);
-		return (
-			<Fragment>
-				<div className="tc">
-					<h1>Robot Friends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
-					<Scroll>
-						<ErrorBoundary>
-							<CardList robots={filteredRobots} />
-						</ErrorBoundary>
-					</Scroll>
-				</div>
-			</Fragment>
-		);
-	}
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+  onSearchChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
+  render() {
+    const { searchfield } = this.state;
+    const filteredRobots = this.state.robots.filter((robot) =>
+      robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    );
+    // console.log(filteredRobots);
+    return (
+      <Fragment>
+        <div className="tc">
+          <h1>Robot Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      </Fragment>
+    );
+  }
 }
